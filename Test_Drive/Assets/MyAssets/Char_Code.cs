ae0f2a0e50fc1a4eb5f8e991776ac5874574c022
Matthew Richins <mattrichins89@gmail.com>
2018-01-27 17:07:47 -0700

    Controls fixed to work for analog stick

--- Test_Drive/Assets/MyAssets/Char_Code.cs
+++ Test_Drive/Assets/MyAssets/Char_Code.cs
@@ -9,10 +9,16 @@ public class Char_Code : MonoBehaviour {
     public float jumpVel = 2.0f;
     public Vector3 jump;
     public Rigidbody rb;
+    public float runForce = 30f;
     public GameObject planet;
     private bool onGround = false;
     public float maxRunSpeed;
     public bool airJump = true;
+    public Collider[] attack_HitBoxes;
+    private bool attackCalled = false;
+    public AudioClip whack;
+    AudioSource audio;
+
 
 
 
@@ -20,17 +26,32 @@ public class Char_Code : MonoBehaviour {
     void Start () {
      
         rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
 
+   
+
         SetMaxRunSpeed();
    
         transform.eulerAngles = Move_sideways();
 
         Jump();
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            LaunchAttack(attack_HitBoxes[0]);
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            if (attackCalled)
+                attackCalled = false;
+        }
 
 	}
 
@@ -48,67 +69,122 @@ public class Char_Code : MonoBehaviour {
 
     Vector3 Move_sideways()
     {
-        if (onGround)
-        {
-            rb.AddRelativeForce(Vector3.right * 6 * Input.GetAxis("Horizontal"));
-        }
-        else
-        {
-            rb.AddRelativeForce(Vector3.right * 3 * Input.GetAxis("Horizontal"));
-           // Vector3 relative_position = rb.transform.position - planet.transform.position;
-        }
+        Vector2 relative_position = rb.transform.position - planet.transform.position;
+ 
+        
 
-        if (onGround && Input.GetAxis("Horizontal") == 0)
-        {
-            GetComponent<Rigidbody>().velocity *= 0;
-        }
 
-        float neededAngle;
-       
-        Vector3 relative_position = rb.transform.position - planet.transform.position;
-       
-            if (relative_position[0] == 0)
-        {
-            if (relative_position[1] > 0)
+        // This is the generic way of moving side to side
+        /**
+            if (onGround)
             {
-                neededAngle = 0;
+
+                rb.AddRelativeForce(Vector3.right * runForce * Input.GetAxis("Horizontal"));
             }
             else
             {
-                neededAngle = 180;
+                rb.AddRelativeForce(Vector3.right * (runForce / 2) * Input.GetAxis("Horizontal"));
+                // Vector3 relative_position = rb.transform.position - planet.transform.position;
+            }
+
+            if (onGround && Input.GetAxis("Horizontal") == 0)
+            {
+                GetComponent<Rigidbody>().velocity *= 0;
+            }
+    **/
+
+ // This is how our charactor will move with analog sticks
+        float angleChar = getAngle(relative_position);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            Vector2 directionRun = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            float angleRunDir = getAngle(directionRun);
+       
+            //check if they desired location is the same as current location
+            if ((int)angleChar + 8 >= (int)angleRunDir && (int)angleRunDir >= (int)angleChar - 8)
+            {
+                //Jump();
+                if (onGround)
+                    rb.velocity = new Vector2(0, 0);
+            }
+            else // Run to given location
+            {
+                float moveMod = 1f;
+                if (!onGround)
+                    moveMod = 0.5f;
+
+
+                if (angleChar>angleRunDir)
+                {
+                    if (angleChar - angleRunDir < 180)
+                        rb.AddRelativeForce(Vector3.right * runForce*moveMod);
+                    else
+                        rb.AddRelativeForce(Vector3.left * runForce*moveMod);
+                }
+                else
+                {
+                    if(angleRunDir - angleChar < 180)
+                        rb.AddRelativeForce(Vector3.left * runForce*moveMod);
+                    else
+                        rb.AddRelativeForce(Vector3.right * runForce*moveMod);
+
+                }
+
             }
         }
-            else if (relative_position[0] > 0)       //check if char is to right of planet
+        
+
+
+                
+            
+        return new Vector3(0, 0, angleChar);
+    }
+
+    float getAngle(Vector2 vector)
+    {
+        float angle;
+
+        if (vector[0] == 0)
         {
-            if (relative_position[1] > 0) //   Quadrant I
+            if (vector[1] > 0)
             {
-                neededAngle = 270 + (Mathf.Atan(relative_position[1] / relative_position[0]) * 180)/Mathf.PI;
+                angle = 0;
+            }
+            else
+            {
+                angle = 180;
+            }
+        }
+        else if (vector[0] > 0)       //check if char is to right of planet
+        {
+            if (vector[1] > 0) //   Quadrant I
+            {
+                angle = 270 + (Mathf.Atan(vector[1] / vector[0]) * 180) / Mathf.PI;
             }
             else   // Quadrant II
             {
-                neededAngle = (Mathf.Atan(relative_position[1] / relative_position[0]) * 180)/ Mathf.PI - 90;
+                angle = 270 + (Mathf.Atan(vector[1] / vector[0]) * 180) / Mathf.PI;
             }
-   
+
         }
         else
         {
-            if(relative_position[1] > 0) //   Quadrant IIV
+            if (vector[1] > 0) //   Quadrant IIV
             {
-                neededAngle = 90 + (Mathf.Atan(relative_position[1] / relative_position[0]) * 180)/ Mathf.PI;
+                angle = 90 + (Mathf.Atan(vector[1] / vector[0]) * 180) / Mathf.PI;
             }
             else  // Quadrand III
             {
-                neededAngle = 90 + (Mathf.Atan(relative_position[1] / relative_position[0]) * 180) / Mathf.PI;
+                angle = 90 + (Mathf.Atan(vector[1] / vector[0]) * 180) / Mathf.PI;
             }
         }
-        return new Vector3(0, 0, neededAngle);
+        return angle;
     }
 
     void OnCollisionEnter(Collision collider)
     {
         onGround = true;
-        //airJump = true;    // moving to when jumped
-        print("Enter called.");
     }
 
    
@@ -117,33 +193,66 @@ public class Char_Code : MonoBehaviour {
         onGround = false;
         airJump = true;
     }
-    
+
+    //This is to convert and return unit vectors
+    Vector2 getUnitVector(float x, float y)
+    {
+        float magnitude = Mathf.Sqrt(x * x + y * y);
+
+        return new Vector2(x / magnitude, y / magnitude);
+    }
 
     void Jump()
     {
        // jump from ground logic
-        if ((onGround | airJump) && Input.GetKeyDown(KeyCode.UpArrow))
+        if ((onGround | airJump) && (Input.GetKeyDown(KeyCode.UpArrow) | Input.GetKeyDown("joystick button 0")))
+        if (onGround | airJump)
         {
             //determine where on planet the Char is
             Vector3 relative_position = rb.transform.position - planet.transform.position;
-           float magnitude = Mathf.Sqrt(relative_position[0] * relative_position[0] + relative_position[1] * relative_position[1]);
+          //DELETE AFTER USE// float magnitude = Mathf.Sqrt(relative_position[0] * relative_position[0] + relative_position[1] * relative_position[1]);
+            Vector2 jumpUnitVector = getUnitVector(relative_position[0], relative_position[1]);
             // jumpDirection is the direction normal to the surface
-            Vector3 jumpDirection = new Vector3((relative_position[0] / magnitude)*jumpVel, (relative_position[1] / magnitude)*jumpVel, 0);
-            print("Jump Direction: " + jumpDirection);
+            Vector3 jumpDirection = new Vector3(jumpUnitVector[0]*jumpVel, jumpUnitVector[1]*jumpVel, 0);
+  
 
             // Get current speed of Char
             Vector3 currentSpeed = GetComponent<Rigidbody>().velocity;
-            print("Current Speed: " + currentSpeed);
+
 
             //if moving we need to combine the current momentum into the jump
             rb.velocity = new Vector3(currentSpeed[0] + jumpDirection[0], currentSpeed[1] + jumpDirection[1], 0);
-            print("Jump Velocity: " + rb.velocity);
+
 
                 onGround = false;
 
             // Air jump logic
             if (airJump)
                 airJump = false;
+        }
+    }
+
+    private void LaunchAttack(Collider collider)
+    {
+        if (attackCalled == false)
+        {
+            attackCalled = true;
+            Collider[] cols = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, collider.transform.rotation, LayerMask.GetMask("HitBox"));
+            foreach (Collider c in cols)
+            {
+
+                if (c.transform.parent == c)
+                    print("I hit myself");
+                else
+                {
+                    print("You hit " + c.name);
+                    //if(c.gameObject == punchBag)
+                    c.GetComponent<Rigidbody>().AddForce((this.transform.position - c.transform.position).normalized *-1000);
+                    audio.Play();
+                
+                }
+                    
+            }
         }
     }
 
---
+++ Test_Drive/Assets/MyAssets/PunchBagBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchBagBehavior : MonoBehaviour {
+
+    private float m_dmgMultiplier = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addDamage(float damage)
+    {
+        m_dmgMultiplier += damage;
+    }
+
+    public float getDamage()
+    {
+        return m_dmgMultiplier;
+    }
+}
