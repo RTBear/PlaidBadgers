220bc2849f37ca6ff3b92df1d79ea109279bf9b8
Matthew Richins <mattrichins89@gmail.com>
2018-02-28 23:55:16 -0700

    Fixed attack. now the opbject being attacked acts as expected...

--- Test_Drive/Assets/MyAssets/Scripts/GameObjectScript.cs
+++ Test_Drive/Assets/MyAssets/Scripts/GameObjectScript.cs
@@ -77,8 +77,8 @@ public class GameObjectScript : MonoBehaviour {
 
         // apply new velocity to char when hit
         Vector3 currentSpeed = rb.velocity;
-        rb.velocity = new Vector3(currentSpeed[0] + LaunchDir[0]*attackForce*launchModifier, currentSpeed[1] + LaunchDir[1]*attackForce*launchModifier, 0);
-        //rb.AddRelativeForce(LaunchDir[0] * attackForce * launchModifier, LaunchDir[0] * attackForce * launchModifier, 0);
+        //rb.velocity = new Vector3(currentSpeed[0] + LaunchDir[0]*attackForce*launchModifier, currentSpeed[1] + LaunchDir[1]*attackForce*launchModifier, 0);
+        rb.AddForce(LaunchDir[0] * attackForce * launchModifier, LaunchDir[1] * attackForce * launchModifier, 0);
     }
 
 
