38fc95d9fb8765621d1c09b1869106833695a61f
Kylie Williamson <kyliewill@rocketmail.com>
2018-04-16 22:15:07 -0600

    Working Mine with particle System. Will have to change the input to whatever it should map to on xbox

--- Test_Drive/Assets/MyAssets/Scripts/SpecialAttackRobot.cs
+++ Test_Drive/Assets/MyAssets/Scripts/SpecialAttackRobot.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class SpecialAttackRobot : SpecialAttack{
 
-	public GameObject Mine = Resources.Load("Prefabs/Mine") as GameObject;
+	public GameObject Mine;
+	GameObject[] AllMines = {null, null, null};
+	int[] WhenMinePlaced = {-1, -1, -1};
+	int currentMine = 0;
+
+	void Awake(){
+		Mine = Resources.Load("Prefabs/Mine") as GameObject;
+	}
 
 	public override bool canSpecialAttack(){
+		
 		return true;
 	}
 
 	public override void specialAttack(){
-		Instantiate (Mine, transform.position, transform.rotation);
+		int smallestMine = currentMine;
+		int mineIndex = -1;
+		bool foundNull = false;
+		for (int i = 0; i < 3; i++) {
+			if (AllMines [i] != null) {
+				if (WhenMinePlaced [i] < smallestMine) {
+					mineIndex = i;
+					smallestMine = WhenMinePlaced [i];
+				}
+			} else {
+				mineIndex = i;
+				break;
+			}
+		}
+		if (!foundNull) {
+			Destroy (AllMines[mineIndex]);
+		}
+		GameObject nextMine = Instantiate (Mine, transform.position, transform.rotation);
+		AllMines [mineIndex] = nextMine;
+		WhenMinePlaced [mineIndex] = currentMine;
+		currentMine++;
+	}
+
+
+	void OnDestroy(){
+		for (int i = 0; i < 3; i++) {
+			Destroy (AllMines[i]);
+		}
 	}
 }
